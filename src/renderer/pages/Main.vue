a9c6d512c742a5482771bf778f98be10bd85454a
shoumen <0showman0@gmail.com>
2021-03-20 20:44:04 +0300

    Fix

--- src/renderer/pages/Main.vue
+++ src/renderer/pages/Main.vue
@@ -73,7 +73,7 @@ export default {
 </script>
 
 <style lang="sass">
-@import "../assets/fonts/roboto/roboto.css"
+//@import "../assets/fonts/roboto/roboto.css"
 @import "../assets/css/all.css"
 @import "../assets/sass/main"
 </style>
